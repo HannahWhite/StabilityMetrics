a4a2efacdf067c6ca8686abf9c25a6a9cc53e4f3
HannahWhite <hannah_wht@hotmail.co.uk>
2019-05-02 16:02:33 +0100

    Create full dataframe of stability metrics

--- Resilience_1km_method2.R
+++ Resilience_1km_method2.R
@@ -3,6 +3,7 @@
 ######################################################################################
 
 ### Hannah White 18.02.2019 
+### Edited 03.04.2019
 
 rm(list=ls())
 
@@ -10,6 +11,8 @@ rm(list=ls())
 
 load('D:\\MODIS6\\MODIS6_18.2.19\\modisv6_1km_all.RData')
 load('J:\\Postdoc Grassland Resilience\\MODIS6\\Anomalies_18.02.2019\\evi.anomaly1km.RData')
+
+load('J:\\Postdoc Grassland Resilience\\MODIS6\\Anomalies_18.02.2019\\evi.anomaly1km.cent.RData')
 
 rm(evi.mean, evi.median, evi.ncell, evi.sd, evi.mad,
    ndvi.mean, ndvi.median, ndvi.ncell, ndvi.sd, ndvi.mad)
@@ -27,11 +30,11 @@ ind2 <- year>=2003 & year<=2019
 evi.variability <- array(NA, dim =c(dim(eastings)))
 
 
-## calculate variability from 2000-2017
+## calculate variability from 2000-2019
 
 for (i in 1:dim(eastings)[1]){
   for (j in 1:dim(eastings)[2]){
-    if (any(is.finite(evi.anomaly[i,j,]))) evi.variability[i,j] <- var(na.omit(evi.anomaly[i,j,]))
+    if (any(is.finite(evi.anomaly.cent[i,j,]))) evi.variability[i,j] <- var(na.omit(evi.anomaly.cent[i,j,]))
     
   }
 }
@@ -43,7 +46,7 @@ for (i in 1:dim(eastings)[1]){
 julian.date2 <- julian.date[ind2]
 modis.dates2 <- modis.dates[ind2]
 
-anomaly.03 <- evi.anomaly[,,ind2] # evi anomaly array just for years 2003-2017
+anomaly.03 <- evi.anomaly[,,ind2] # evi anomaly array just for years 2003-2019
 
 evi.magnitude <- array(NA, dim=c(dim(eastings)))
 
@@ -130,6 +133,8 @@ try(
   }
 )
 
+evi.magnitude.median <- apply(evi.all.events, c(1,2), median, na.rm = TRUE) # calculates median of all drops below -1 sd
+
 save(evi.magnitude, evi.events, evi.dates, evi.all.events, evi.variability, file = 'J:\\Postdoc Grassland Resilience\\MODIS6\\Resilience\\resilience_measures_1km.RData')
 
 
@@ -143,7 +148,7 @@ save(evi.magnitude, evi.events, evi.dates, evi.all.events, evi.variability, file
 mov.avg <- array(NA, dim = c(dim(eastings),(dim(anomaly.03)[3])-5))
 
 
-for (kk in 1:685){ #begins moving window at each separate time point
+for (kk in 1:734){ #begins moving window at each separate time point
   window.frame <- anomaly.03[,,kk:(kk+5)] # moving window of 6 units
   mov.avg[,,kk] <- apply(window.frame, c(1,2), mean, na.rm = TRUE)
   
@@ -180,9 +185,9 @@ for (ii in 1:dim(eastings)[1]){
           d.day <- date.event.finite[mm]
           start <- which(modis.dates2==d.day)
           
-          if (start<=684) {
+          if (start<=733) {
             
-            return.time <- which(mov.avg[ii, jj, (start+1):685]>=0)[1] # calculates recovery time in units of time windows
+            return.time <- which(mov.avg[ii, jj, (start+1):734]>=0)[1] # calculates recovery time in units of time windows
             # needs to be start + 1 so that event window isn't counted
             
             evi.all.rectime[ii, jj, mm] <- return.time
@@ -208,8 +213,247 @@ for (ii in 1:dim(eastings)[1]){
 
 
 evi.rectime <- apply(evi.all.rectime, c(1,2), mean, na.rm = TRUE)  # calculates mean return time 
-evi.rate <- apply(evi.all.rate, c(1,2), mean, na.rm = TRUE)
+evi.rectime.median <- apply(evi.all.rectime, c(1,2), median, na.rm = TRUE) #calculates median return time
+evi.rectime.big <- evi.all.rectime[,,1] # recovery time from biggest drop
+
+evi.rate <- apply(evi.all.rate, c(1,2), mean, na.rm = TRUE) # calculates mean rate
+evi.rate.median <- apply(evi.all.rate, c(1,2), median, na.rm = TRUE) # calculates median rate
+evi.rate.big <- evi.all.rate[,,1] # calculates rate of return from biggest drop
+
+
 
 save(evi.magnitude, evi.events, evi.dates, evi.all.events, evi.variability, 
-     evi.all.rectime, evi.all.rate, evi.rectime, evi.rate, file = 'J:\\Postdoc Grassland Resilience\\MODIS6\\Resilience\\resilience_measures_1km.RData')
+     evi.all.rectime, evi.all.rate, evi.rectime, evi.rate, evi.rectime.median, evi.rectime.big, evi.rate.median, evi.rate.big, 
+     evi.longest.rec, evi.slowest.rate, evi.longest.date, evi.slowest.date,
+     file = 'J:\\Postdoc Grassland Resilience\\MODIS6\\Resilience\\resilience_measures_1km.RData')
+
+
+
+### Cross Correlations
+
+## Calculate and evi.drop which is the biggest drop in each site
+
+evi.drop <- apply(evi.all.events, c(1,2), min, na.rm = TRUE) # has to be min as currently negative
+
+
+
+## Firstly for variability and mean measures across time periods for others
+
+# Get mean measures and variability into dataframe
+
+## var
+evi.var <- evi.variability[which(is.finite(evi.variability))]
+eastings.var <- eastings[which(is.finite(evi.variability))]
+northings.var <- northings[which(is.finite(evi.variability))]
+
+evi.var <- data.frame(evi.var, eastings = eastings.var, northings = northings.var)
+
+
+## resistance
+evi.mag <- evi.magnitude[which(is.finite(evi.magnitude))]
+eastings.mag <- eastings[which(is.finite(evi.magnitude))]
+northings.mag <- northings[which(is.finite(evi.magnitude))]
+
+evi.mag <- data.frame(evi.mag, eastings = eastings.mag, northings = northings.mag)
+
+## median resistance
+evi.magmed <- evi.magnitude.median[which(is.finite(evi.magnitude.median))]
+eastings.magmed <- eastings[which(is.finite(evi.magnitude.median))]
+northings.magmed <- northings[which(is.finite(evi.magnitude.median))]
+
+evi.magmed <- data.frame(evi.magmed = evi.magmed, eastings = eastings.magmed, northings = northings.magmed)
+
+## max drop
+evi.maxdrop <- evi.drop[which(is.finite(evi.drop))]
+eastings.maxdrop <- eastings[which(is.finite(evi.drop))]
+northings.maxdrop <- northings[which(is.finite(evi.drop))]
+
+evi.maxdrop <- data.frame(evi.drop = evi.maxdrop, eastings = eastings.maxdrop, northings = northings.maxdrop)
+
+## recovery time
+evi.rec <- evi.rectime[which(is.finite(evi.rectime))]
+eastings.rec <- eastings[which(is.finite(evi.rectime))]
+northings.rec <- northings[which(is.finite(evi.rectime))]
+
+evi.rec <- data.frame(evi.rec, eastings = eastings.rec, northings = northings.rec)
+
+## recovery time median
+evi.recmed <- evi.rectime.median[which(is.finite(evi.rectime.median))]
+eastings.recmed <- eastings[which(is.finite(evi.rectime.median))]
+northings.recmed <- northings[which(is.finite(evi.rectime.median))]
+
+evi.rec.med<- data.frame(evi.recmed, eastings = eastings.recmed, northings = northings.recmed)
+
+## recovery time from biggest drop
+evi.rec.big <- evi.rectime.big[which(is.finite(evi.rectime.big))]
+eastings.recbig <- eastings[which(is.finite(evi.rectime.big))]
+northings.recbig <- northings[which(is.finite(evi.rectime.big))]
+
+evi.rec.big <- data.frame(evi.rec.big, eastings = eastings.recbig, northings = northings.recbig)
+
+## rate
+evi.recrate <- evi.rate[which(is.finite(evi.rate))]
+eastings.recrate <- eastings[which(is.finite(evi.rate))]
+northings.recrate <- northings[which(is.finite(evi.rate))]
+
+evi.recrate <- data.frame(evi.recrate, eastings = eastings.recrate, northings = northings.recrate)
+
+
+## rate median
+evi.recrate.med <- evi.rate.median[which(is.finite(evi.rate.median))]
+eastings.recrate.med <- eastings[which(is.finite(evi.rate.median))]
+northings.recrate.med <- northings[which(is.finite(evi.rate.median))]
+
+evi.recrate.med <- data.frame(evi.recrate.med, eastings = eastings.recrate.med, northings = northings.recrate.med)
+
+
+## rate from biggest drop
+evi.recrate.big <- evi.rate.big[which(is.finite(evi.rate.big))]
+eastings.recratebig <- eastings[which(is.finite(evi.rate.big))]
+northings.recratebig <- northings[which(is.finite(evi.rate.big))]
+
+evi.recrate.big <- data.frame(evi.recrate.big, eastings = eastings.recratebig, northings = northings.recratebig)
+
+
+
+## longest return
+evi.long <- evi.longest.rec[which(is.finite(evi.longest.rec))]
+eastings.long <- eastings[which(is.finite(evi.longest.rec))]
+northings.long <- northings[which(is.finite(evi.longest.rec))]
+
+evi.long <- data.frame(evi.long, eastings = eastings.long, northings = northings.long)
+
+## slowest rate
+evi.slow <- evi.slowest.rate[which(is.finite(evi.slowest.rate))]
+eastings.slow <- eastings[which(is.finite(evi.slowest.rate))]
+northings.slow <- northings[which(is.finite(evi.slowest.rate))]
+
+evi.slow <- data.frame(evi.slow, eastings = eastings.slow, northings = northings.slow)
+
+
+### Merge
+
+
+stability1km <- merge(evi.var, evi.mag, by = c('eastings', 'northings'), all = TRUE)
+stability1km <- merge(stability1km, evi.magmed, by = c('eastings', 'northings'), all = TRUE)
+stability1km <- merge(stability1km, evi.rec, by = c('eastings', 'northings'), all = TRUE)
+stability1km <- merge(stability1km, evi.recrate, by = c('eastings', 'northings'), all = TRUE)
+stability1km <- merge(stability1km, evi.maxdrop, by = c('eastings', 'northings'), all = TRUE)
+
+stability1km <- merge(stability1km, evi.rec.med, by = c('eastings', 'northings'), all = TRUE)
+stability1km <- merge(stability1km, evi.rec.big, by = c('eastings', 'northings'), all = TRUE)
+stability1km <- merge(stability1km, evi.recrate.med, by = c('eastings', 'northings'), all = TRUE)
+stability1km <- merge(stability1km, evi.recrate.big, by = c('eastings', 'northings'), all = TRUE)
+
+
+stability1km <- merge(stability1km, evi.long, by = c('eastings', 'northings'), all = TRUE)
+stability1km <- merge(stability1km, evi.slow, by = c('eastings', 'northings'), all = TRUE)
+
+
+# change magnitude to inverse of the absolute number
+
+stability1km$evi.mag <- 1/abs(stability1km$evi.mag)
+stability1km$evi.magmed <- 1/abs(stability1km$evi.magmed)
+stability1km$evi.drop <- 1/abs(stability1km$evi.drop)
+
+write.csv(stability1km, 'J:\\Postdoc Grassland Resilience\\MODIS6\\Resilience\\stability1km.csv', row.names = FALSE)
+
+#cor(stability1km[,3:7], use = 'na.or.complete', method = 'spearman') # use Spearman's as many correlations appear non linear (see pair graph)
+
+
+#panel.cor <- function(x, y, ...)
+#{
+#  par(usr = c(0, 1, 0, 1))
+#  txt <- as.character(format(cor(x, y, use = 'na.or.complete', method = 'spearman'), digits=2))
+#  text(0.5, 0.5, txt, cex = 4)
+#}
+
+#pairs(stability1km[,3:7], labels = c('variability', 'resistance', 'recovery time', 'recovery rate', 'biggest perturbation'), 
+#      upper.panel = panel.cor)
+
+
+#library(Hmisc)
+
+#rcorr(cbind(stability1km[,3], stability1km[,4], stability1km[,5], stability1km[,6], stability1km[,7]),  type = 'spearman') ## all significant
+
+
+### Plot dates when events are happening
+
+### ggplot histogram
+#library(ggplot2)
+#library(scales)
+#library(lubridate)
+
+
+#### All events
+
+## stack values
+#dd <- evi.dates[which(!is.na(evi.dates))]
+#ddnum <- as.numeric(dd)
+
+#dd.df <- data.frame(dd, ddnum)
+
+#bin = 30
+
+#p <- ggplot(dd.df, aes(dd, ..count..))
+#p <- p + geom_histogram(data = dd.df, binwidth = bin)
+
+# The numeric data is treated as a date,
+# breaks are set to an interval equal to the binwidth,
+# and a set of labels is generated and adjusted in order to align with bars
+#p <- p + scale_x_date(breaks = waiver(),
+#                      date_breaks = '1 year',
+#                      labels = date_format("%Y-%m"),
+#                      limits = c(as.Date("2003-01-01"), 
+#                                 as.Date("2019-01-25")))
+#p <- p + xlab('')
+#p <- p + scale_fill_manual(values = c(rep(c('grey20', 'grey80'), 7), 'grey20'))
+
+# from here, format at ease
+#p <- p + theme(axis.text.x  = element_text(angle=45, hjust = 1, vjust = 1, size = 14), axis.text.y = element_text(size = 11), legend.position ='none',
+#               panel.background = element_blank(), panel.grid = element_blank(), axis.line = element_line(colour = 'black')) + ylab('')
+
+#p
+
+
+#### Largest events
+
+#evi.dates.big <- evi.dates[,,1]
+#ddbig <- evi.dates.big[which(!is.na(evi.dates.big))]
+#ddnumbig <- as.numeric(ddbig)
+
+#ddbig.df <- data.frame(ddbig, ddnumbig)
+
+
+#p2 <- ggplot(ddbig.df, aes(x = ddbig, y = ..count..))
+#p2 <- p2 + geom_histogram(data = ddbig.df, binwidth = 30)
+
+# The numeric data is treated as a date,
+# breaks are set to an interval equal to the binwidth,
+# and a set of labels is generated and adjusted in order to align with bars
+#p2 <- p2 + scale_x_date(breaks = waiver(),
+#                        date_breaks = '1 year',
+#                        labels = date_format("%Y-%m"),
+#                        limits = c(as.Date("2003-01-01"), 
+#                                   as.Date("2019-01-25")))
+#p2 <- p2 + xlab('')
+#p2 <- p2 + scale_fill_manual(values = c(rep(c('grey20', 'grey80'), 7), 'grey20'))
+
+# from here, format at ease
+#p2 <- p2 + theme(axis.text.x  = element_text(angle=45, hjust = 1, vjust = 1, size = 14), axis.text.y = element_text(size = 11), legend.position ='none',
+#                 panel.background = element_blank(), panel.grid = element_blank(), axis.line = element_line(colour = 'black')) + ylab('')
+
+#p2
+
+
+#library(cowplot)
+
+#tiff('J:\\Postdoc Grassland Resilience\\Writing\\MODIS_resilience_metrics\\Graphics\\events1km.tiff', width = 14, height = 7, units = 'in', res = 360)
+#plot_grid(p, p2, nrow = 1, labels = c('a)', 'b)'))
+#dev.off()
+
+
+
+
+
 
